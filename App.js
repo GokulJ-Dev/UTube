2e88298355bfe9503e0f4e44b883b00552da40bc
Gokul <xpand.developer@gmail.com>
2023-04-07 14:01:09 +0530

    EP-2 Ignition

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const parent = React.createElement("div", { id: "Parent" },
     React.createElement("div", { id: "Child" }, [
         React.createElement("h1", { id: "h1Tag" }, "Hello Gokul From React"), React.createElement("h4", { id: "h4Tag" }, "Rock it mannn!")
